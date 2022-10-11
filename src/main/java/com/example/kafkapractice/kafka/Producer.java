437a21ee74ef089612bb4413e6d27ef703b74e39
송민진 <m_6595@naver.com>
2022-10-12 03:55:22 +0900

    #1 refactor : 프로듀서 주석 작성

--- src/main/java/com/example/kafkapractice/kafka/Producer.java
+++ src/main/java/com/example/kafkapractice/kafka/Producer.java
@@ -10,13 +10,13 @@ public class Producer {
     public static void main(String[] args) throws IOException {
 
         Properties configs = new Properties();
-        configs.put("bootstrap.servers", "localhost:9092");
-        configs.put("key.serializer", "org.ap            ache.kafka.common.serialization.StringSerializer");
-        configs.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        configs.put("bootstrap.servers", "localhost:9092");        // 브로커 지정 - 실무에서는 여러개 지정하기
+        configs.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");        // 키 직렬화
+        configs.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");      // 밸류 직렬화
 
-        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(configs);
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(configs);        // 프로듀서 인스턴스 생성 - 얘로 인해 데이터를 전송
 
-        ProducerRecord<String, String> record = new ProducerRecord<String, String>("click_log", "login");
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("click_log", "login");       // 토픽 지정 + 보낼 데이터 값 지정
         // 만약 key를 같이 보내고 싶은 경우, 아래와 같이 작성
         // ProducerRecord<String, String> record = new ProducerRecord<String, String>("click_log", "1", "login");
 
